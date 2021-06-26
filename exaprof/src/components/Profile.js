f2a8ef9491fdb65288baa4af0d03816c854b15be
VictorviruzZ2 <victorvf13.vvf@gmail.com>
2021-06-26 09:49:17 -0400

    Profile realizado

---
+++ exaprof/src/components/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './styles/Profile.css';
+export default function Categories() {
+    return (
+        <>
+            <div className = "conteiner categories">
+                <div className = "card"></div>
+            <div className = "img1">
+            <img src = "" alt=""/></div>
+            <div className = "cardg"></div>
+            <div className = "plants"></div>
+            <div className = "white"></div>
+            <div className = "elipse"></div>
+            <div className = "foto"></div>
+            <p className="nombre">Yarriba Castro</p>
+            <p className="text1">Design Track</p>
+            <p className="text2">I chose design track because I love to design beautiful user-centered interfaces. </p>
+            <p className="text3">Yaretas24 </p>
+            <p className="text4">Yaretas24 </p>
+            <div className = "in"></div>
+            <div className = "te"></div>
+            </div>
+        </>
+    )
+}
